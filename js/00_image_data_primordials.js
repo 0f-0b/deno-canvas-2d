a52f105ee47f759b639a820fc097cb67f8d1f723
ud2 <sjx233@qq.com>
2024-02-07 13:34:52 +0800

    `ImageData` is moved back to `deno_web`

--- js/00_image_data_primordials.js
+++ js/00_image_data_primordials.js
@@ -1,19 +1,11 @@
 import { primordials } from "ext:core/mod.js";
+import { ImageDataPrototype } from "ext:deno_web/16_image_data.js";
 
 const { ObjectGetOwnPropertyDescriptors, uncurryThis } = primordials;
-export let ImageData;
-export let ImageDataPrototype;
-export let ImageDataPrototypeGetColorSpace;
-export let ImageDataPrototypeGetData;
-export let ImageDataPrototypeGetHeight;
-export let ImageDataPrototypeGetWidth;
-
-export function setImageData(constructor) {
-  ImageData = constructor;
-  ImageDataPrototype = ImageData.prototype;
-  const proto = ObjectGetOwnPropertyDescriptors(ImageDataPrototype);
-  ImageDataPrototypeGetColorSpace = uncurryThis(proto.colorSpace.get);
-  ImageDataPrototypeGetData = uncurryThis(proto.data.get);
-  ImageDataPrototypeGetHeight = uncurryThis(proto.height.get);
-  ImageDataPrototypeGetWidth = uncurryThis(proto.width.get);
-}
+const proto = ObjectGetOwnPropertyDescriptors(ImageDataPrototype);
+export const ImageDataPrototypeGetColorSpace = uncurryThis(
+  proto.colorSpace.get,
+);
+export const ImageDataPrototypeGetData = uncurryThis(proto.data.get);
+export const ImageDataPrototypeGetHeight = uncurryThis(proto.height.get);
+export const ImageDataPrototypeGetWidth = uncurryThis(proto.width.get);
